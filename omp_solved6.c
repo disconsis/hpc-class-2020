6703fb9e26b48476b7e4809825002d2b3edb7aee
disconsis <ketan.kanishka.cse15@iitbhu.ac.in>
2020-03-13 01:59:09 -0400

    solve omp bug 6

--- omp_solved6.c
+++ omp_solved6.c
@@ -11,20 +11,37 @@
 #include <stdlib.h>
 #define VECLEN 100
 
+/** Solution
+ *
+ * There were a couple of bugs in this.
+ * First of all, it didn't compile since dotprod didn't return anything.
+ * So I took sum as an in-variable.
+ *
+ * Secondly, dotprod was called from a parallel omp section, thus each
+ * thread executed it independently, which is probably not the intended behaviour.
+ * So I moved the parallel section inside the function.
+ *
+ * I assume the question had something to do with sum being a shared variable,
+ * but since the code didn't compile as-is I didn't really know what was expected
+ * in that scenario.
+ */
+
 float a[VECLEN], b[VECLEN];
 
-float dotprod ()
+void dotprod (float& sum)
 {
 int i,tid;
-float sum;
 
-tid = omp_get_thread_num();
-#pragma omp for reduction(+:sum)
-  for (i=0; i < VECLEN; i++)
-    {
+#pragma omp parallel private(tid)
+{
+  tid = omp_get_thread_num();
+
+  #pragma omp for reduction(+ : sum)
+  for (i=0; i<VECLEN; i++) {
     sum = sum + (a[i]*b[i]);
     printf("  tid= %d i=%d\n",tid,i);
-    }
+  }
+}
 }
 
 
@@ -36,8 +53,7 @@ for (i=0; i < VECLEN; i++)
   a[i] = b[i] = 1.0 * i;
 sum = 0.0;
 
-#pragma omp parallel shared(sum)
-  dotprod();
+dotprod(sum);
 
 printf("Sum = %f\n",sum);
 
